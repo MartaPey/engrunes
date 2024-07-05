1df213f278e28cb8df08907f0f86922769c2b2de
Marta Pey <mapero2001@gmail.com>
2024-07-05 16:41:17 +0200

    lo envio?

--- js/style.js
+++ js/style.js
@@ -1,34 +1,36 @@
 function on() {
-    
+
 }
 
 function off() {
-     document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
 
- 
+
 // Drag carpetas
- const draggable = document.getElementById('draggable');
-    const header = document.getElementById('draggable-header');
-    let offsetX, offsetY;
+const draggable = document.getElementById('draggable');
+const header = document.getElementById('draggable-header');
+let offsetX, offsetY;
 
-    header.addEventListener('mousedown', (e) => {
-        offsetX = e.clientX - draggable.getBoundingClientRect().left;
-        offsetY = e.clientY - draggable.getBoundingClientRect().top;
-        header.style.cursor = 'grabbing';
+header.addEventListener('mousedown', (e) => {
+    offsetX = e.clientX - draggable.getBoundingClientRect().left;
+    offsetY = e.clientY - draggable.getBoundingClientRect().top;
+    header.style.cursor = 'grabbing';
 
-        function onMouseMove(e) {
-            draggable.style.left = `${e.clientX - offsetX}px`;
-            draggable.style.top = `${e.clientY - offsetY}px`;
-        }
+    function onMouseMove(e) {
+        draggable.style.left = `${e.clientX - offsetX}px`;
+        draggable.style.top = `${e.clientY - offsetY}px`;
+    }
 
-        document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mousemove', onMouseMove);
 
-        document.addEventListener('mouseup', () => {
-            document.removeEventListener('mousemove', onMouseMove);
-            header.style.cursor = 'grab';
-        }, { once: true });
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        header.style.cursor = 'grab';
+    }, {
+        once: true
     });
+});
 
 //Darkmode
 
@@ -43,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     modeToggleButton.addEventListener('click', () => {
         document.getElementById('cuerpo').classList.toggle('dark-mode');
-        
+
         // Save the current mode in localStorage
         const isDarkMode = document.getElementById('cuerpo').classList.contains('dark-mode');
         localStorage.setItem('mode', isDarkMode ? 'dark' : 'light');
@@ -54,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //Cambiar color boton a lila al hacer click
 
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleElements =document.getElementById('toggle-element');
+    const toggleElements = document.getElementById('toggle-element');
 
     toggleElements.forEach(element => {
         element.addEventListener('click', () => {
@@ -127,80 +129,73 @@ document.querySelector('.habilitats-button').addEventListener('click', toggleHab
 //guardiola
 
 function toggleImages1() {
-            const image1 = document.getElementById('guardiola1');
-            const image2 = document.getElementById('guardiola2');
- 
+    const image1 = document.getElementById('guardiola1');
+    const image2 = document.getElementById('guardiola2');
 
-            if (image1.style.display === 'none') {
-                image1.style.display = 'block';
-                image2.style.display = 'none';
-            } else {
-                image1.style.display = 'none';
-                image2.style.display = 'block';
-            }
-        }
+
+    if (image1.style.display === 'none') {
+        image1.style.display = 'block';
+        image2.style.display = 'none';
+    } else {
+        image1.style.display = 'none';
+        image2.style.display = 'block';
+    }
+}
 
 //radiografia
 function toggleImages2() {
-            const image3 = document.getElementById('radiografia1');
-            const image4 = document.getElementById('radiografia2');
- 
+    const image3 = document.getElementById('radiografia1');
+    const image4 = document.getElementById('radiografia2');
 
-            if (image3.style.display === 'none') {
-                image3.style.display = 'block';
-                image4.style.display = 'none';
-            } else {
-                image3.style.display = 'none';
-                image4.style.display = 'block';
-            }
-        }
+
+    if (image3.style.display === 'none') {
+        image3.style.display = 'block';
+        image4.style.display = 'none';
+    } else {
+        image3.style.display = 'none';
+        image4.style.display = 'block';
+    }
+}
 
 
 //concert
- document.getElementById('slider').addEventListener('input', function() {
-            var sliderValue = this.value;
-            var concert2 = document.getElementById('concert2');
-            concert2.style.opacity = sliderValue / 100;
-        });
-
-
-
-
-
+document.getElementById('slider').addEventListener('input', function () {
+    var sliderValue = this.value;
+    var concert2 = document.getElementById('concert2');
+    concert2.style.opacity = sliderValue / 100;
+});
 
 
 //Hora
 
-       function startTime()
-{
-var today=new Date();
-var h=today.getHours();
-var m=today.getMinutes();
-var s=today.getSeconds();
-// add a zero in front of numbers<10
-m=checkTime(m);
-s=checkTime(s);
-document.getElementById('txt').innerHTML=h+":"+m;
-t=setTimeout('startTime()',500);
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m;
+    t = setTimeout('startTime()', 500);
 }
-function checkTime(i)
-{
-if (i<10)
-{
-i="0" + i;
-}
-return i;
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
 
 //Numero random
 function generateRandomNumber() {
-            return Math.floor(Math.random() * 100); // Genera un número aleatorio entre 0 y 99
-        }
+    return Math.floor(Math.random() * 100); // Genera un número aleatorio entre 0 y 99
+}
 
-        function updateRandomNumber() {
-            var randomNumberElement = document.getElementById('random-number');
-            randomNumberElement.textContent = generateRandomNumber();
-        }
+function updateRandomNumber() {
+    var randomNumberElement = document.getElementById('random-number');
+    randomNumberElement.textContent = generateRandomNumber();
+}
 
-        // Actualiza el número aleatorio cada segundo (1000 milisegundos)
-        setInterval(updateRandomNumber, 100);
+// Actualiza el número aleatorio cada segundo (1000 milisegundos)
+setInterval(updateRandomNumber, 100);
